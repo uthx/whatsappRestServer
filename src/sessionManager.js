54b54f0d3660c334998f9c7f95be6f4dd65e5116
uthx <rdrahuldhiman.19@gmail.com>
2023-01-27 18:11:54 +0530

    Updated puppeteer config

--- src/sessionManager.js
+++ src/sessionManager.js
@@ -1,30 +1,34 @@
 // import {Client, LocalAuth} from 'whatsapp-web.js';
-import WhatsappClient from 'whatsapp-web.js';
-import qrCode from 'qrcode-terminal';
+import WhatsappClient from "whatsapp-web.js";
+import qrCode from "qrcode-terminal";
 export const sessionManager = (sessionId) => {
-    console.log("started")
-    console.log("sesisonId",sessionId);
-    const client = new WhatsappClient.Client({
-        authStrategy: new WhatsappClient.LocalAuth({
-            clientId: sessionId
-        })
-    })
-    client.on('authenticated', () => {
-        console.log(`Client: ${sessionId} is Authenticated`);
-    });
+  console.log("started");
+  console.log("sesisonId", sessionId);
+  const client = new WhatsappClient.Client({
+    puppeteer: {
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    },
+    authStrategy: new WhatsappClient.LocalAuth({
+      clientId: sessionId,
+    }),
+  });
+  client.on("authenticated", () => {
+    console.log(`Client: ${sessionId} is Authenticated`);
+  });
 
-    client.on('qr', code => {
-        console.log(` Generating QE for ${sessionId}`)
-        qrCode.generate(code, {small: true})
-    });
+  client.on("qr", (code) => {
+    console.log(` Generating QE for ${sessionId}`);
+    qrCode.generate(code, { small: true });
+  });
 
-    client.on('ready', () => {
-        console.log(`Client : ${sessionId} is ready.`)
-    })
-    client.on('message', message => {
-        console.log(`Client: ${sessionId} received a mesage`, message.body);
-    })
-    client.initialize();
+  client.on("ready", () => {
+    console.log(`Client : ${sessionId} is ready.`);
+  });
+  client.on("message", (message) => {
+    console.log(`Client: ${sessionId} received a mesage`, message.body);
+  });
+  client.initialize();
 
-    return client
-}
+  return client;
+};
