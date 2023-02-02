c62767638608eadfa4527a5264a0e8323de26871
uthx <rdrahuldhiman.19@gmail.com>
2023-02-03 01:02:13 +0530

    Improved Event Listening Flow

--- src/index.js
+++ src/index.js
@@ -1,60 +1,8 @@
-// import express from "express";
-// import qrCode from 'qrcode-terminal';
-// import WhatsappClient from "whatsapp-web.js";
-// import bodyParser from 'body-parser';
-// import { route } from "../src/routes.js";
-// const app = express();
-// const port = 3000;
-
-
-// export const client = new WhatsappClient.Client({
-//     authStrategy: new WhatsappClient.LocalAuth({
-//         clientId: "client1",
-//     })
-// })
-// console.log({client: client.options.puppeteer})
-// client.on('authenticated',(sessionData) => {
-//     console.log("sessionData",sessionData)
-// })
-// app.use(express.json());
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-
-// client.on('qr', code => {
-//     qrCode.generate(code, {small: true})
-// })
-
-// // Client ready
-
-// client.on('ready', () => {
-//     console.log('Client is ready');
-// })
-
-// client.on('message', message => {
-//     console.log("messsage", message)
-//     console.log("Message received", message.body);
-// })
-// client.initialize()
-// process.on("SIGINT", async () => {
-//     console.log("(SIGINT) Shutting down...");
-//     await client.destroy();
-//     process.exit(0);
-// })
-
-// app.use('/', route)
-
-// app.listen(port, () => {
-//     console.log(`Express Server listing at port ${port}`)
-// })
-
-
 import express from "express";
-import qrCode from 'qrcode-terminal';
 import bodyParser from 'body-parser';
 import { route } from "../src/routes.js";
 import { sessionManager } from "./sessionManager.js";
-// import qrCode from 'qrcode-terminal';
+import { clientSessionHelper } from "./utils/helpers.js";
 const app = express();
 const port = 3000;
 
@@ -64,51 +12,40 @@ app.use(bodyParser.urlencoded({
 }));
 
 export const clientSessionStore = {};
-// console.log(clientSessionStore);
-app.post('/initSession/:phone', (req, res) => {
+app.post('/initSession/:phone', async (req, res) => {
+    console.log("initSession started");
     try {
         const phoneNumber = req.params.phone;
-        let sent;
-        let generatedQrCode;
-        let message;
-        // console.log(`initSession input`, phoneNumber);
+        const existingClient = clientSessionStore[phoneNumber];
+        const clientState = await existingClient?.getState();
+        console.log({
+            clientState
+        })
+        if(clientState === "CONNECTED") {
+            return res.send({
+                 "message": "User is already authenticated",
+             })
+         }
         const clientInstance = sessionManager(phoneNumber);
-        clientInstance.on('qr', code => {
-            console.log("Flow in qr code")
-            if(!sent) {
-                sent = true;
-                generatedQrCode = code
-                message = "QR Code Attached"
-                console.log("QR Code Attached")
-                // qrCode.generate(code , {small: true})
-                return res.send({
-                    message,
-                    generatedQrCode
-                })
-            } else {
-                console.log("User is already authenticated")
-            }
-        })
-        clientInstance.on('authenticated', () => {
-            // user is already authenticated
-            console.log("Flow in auth check")
-            if(!sent) {
-                sent = true
-                message = "Authentication complete"
-                console.log("Authentication complete")
-               return res.send({
-                    message,
-                    generatedQrCode
-                })
-            } else {
-                console.log("User is already Authenticated")
-            }
-        })
-        clientSessionStore[phoneNumber] = clientInstance
-        // console.log({clientSessionStore});
+        clientSessionStore[phoneNumber] = clientInstance;
+        const resp = await clientSessionHelper(clientInstance)
+        console.log({resp})
+        
+        if(resp.type === 'qr') {
+            return res.send({
+                "message": "QR Code Attached",
+                "generatedQrCode": resp.code
+            })
+        } else if(resp.type === 'authenticated' || clientState === "CONNECTED") {
+           return res.send({
+                "message": "User is already authenticated",
+            })
+        } else {
+            throw new Error(`Unexpected resp.type received : ${resp.type}`)
+        }
     } catch (error) {
         console.log("initSession error", error);
-        res.send("negative");
+        return res.send("negative");
     }
 })
 app.use('/', route)
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9,3 +9,29 @@ export const getClientInstance = (sessionId) => {
     if(!client) throw new Error(`getClientInstance: No existing session found for clientSessionId : ${sessionId}`);
     return client;
 }
+
+export const clientSessionHelper = async (client) => {
+    return new Promise((resolve, reject) => {
+        client.on('qr', code => {
+            console.log("Event Type: qr");
+            return resolve({type: 'qr', code})
+        })
+        client.on('authenticated', () => {
+            console.log("Event Type: authenticated")
+            return resolve({type: 'authenticated'})
+        })
+        client.on('disconnected', () => {
+            console.log("Event Type: disconnected")
+            // return resolve({type: 'disconnected'})
+        })
+        client.on('auth_failure', () => {
+            console.log("Event Type: auth_failure")
+            // return resolve({type: 'auth_failure'})
+        })
+        client.on('ready', () => {
+            console.log("Event Type: ready")
+            // return resolve({type: 'ready'})
+        })
+    })
+}
+
